4596701588cd80a00f072f25f8ab37b984d17473
Tim Matheson <t.matheson@acrrm.org.au>
2021-03-29 11:43:24 +1000

    #3 setup bootstrap.

--- tezziewezzie/src/components/header.tsx
+++ tezziewezzie/src/components/header.tsx
@@ -2,6 +2,7 @@
 
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
+import imgIcon from '../img/TS_blu.png';
 
 export const Header = () => {
  return (
@@ -43,23 +44,15 @@ export const Header = () => {
    </div>
    <div className="navbar navbar-dark bg-dark box-shadow">
     <div className="container d-flex justify-content-between">
-     <a href="#" className="navbar-brand d-flex align-items-center">
-      <svg
-       xmlns="[http://www.w3.org/2000/svg](http://www.w3.org/2000/svg)"
-       width="20"
-       height="20"
-       viewBox="0 0 24 24"
-       fill="none"
-       stroke="currentColor"
-       strokeWidth="2"
-       strokeLinecap="round"
-       strokeLinejoin="round"
-       className="mr-2"
-      >
-       <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z" />
-       <circle cx="12" cy="13" r="4" />
-      </svg>
-      <strong>Album</strong>
+    <a href="#" className="navbar-brand d-flex align-items-center">
+     <img
+     src={imgIcon}
+     alt="Card"
+     style={{width: 100, height: 100, position: 'absolute'}}
+    />
+    </a>
+    <a href="#" className="navbar-brand d-flex align-contents-center">
+    <strong>.TezzieWezzie</strong>
      </a>
      <button
       className="navbar-toggler"
@@ -68,8 +61,7 @@ export const Header = () => {
       data-target="#navbarHeader"
       aria-controls="navbarHeader"
       aria-expanded="false"
-      aria-label="Toggle navigation"
-     >
+      aria-label="Toggle navigation"     >
       <span className="navbar-toggler-icon" />
      </button>
     </div>
